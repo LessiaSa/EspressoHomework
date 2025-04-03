aef7cf6537fd9111d13ae3d1ab3bbbafef426d1a
LessiaSa <0162221@mail.ru>
2025-04-03 16:34:33 +0700

    IgnoreCached

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTestHomework.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoTestHomework.java
@@ -1,15 +1,12 @@
 package ru.kkuzmichev.simpleappforespresso;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
